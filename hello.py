13ab8c75044b223a1e6c78061ea37b25bd04e0b9
“kenluo001” <ken.luo.5227@gmail.com>
2023-09-16 16:03:42 +0800

    test again

--- hello.py
+++ hello.py
@@ -1,1 +1,25 @@
 print("hello")
+print('hello, world!')
+import sys
+
+print(sys.version_info)
+print(sys.version)
+
+print("-------------------------")
+
+# import this
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(200)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
